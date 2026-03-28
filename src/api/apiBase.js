b5232857db7e404925b600043e45a61075125631
Dipanshu batra <devbatra887@gmail.com>
2026-03-28 14:44:45 +0530

    Update apiBase.js

--- src/api/apiBase.js
+++ src/api/apiBase.js
@@ -1,2 +1,2 @@
-export const API_BASE = "https://bookmark-backend-z38v.onrender.com";
+export const API_BASE = import.meta.env.VITE_API_BASE || "https://bookmark-backend-z38v.onrender.com";
 export const API_PREFIX = "/api";
